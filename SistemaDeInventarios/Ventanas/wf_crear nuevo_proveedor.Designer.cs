cc83c9194c0fcb331c4cd47ad1b83e14f46f0ae4
jrmorenoRM <jrmoreno10jr@gmail.com>
2019-11-18 17:20:45 -0600

    FORMULARIO CONTACTO PROVEEDORES

--- SistemaDeInventarios/Ventanas/wf_crear nuevo_proveedor.Designer.cs
+++ SistemaDeInventarios/Ventanas/wf_crear nuevo_proveedor.Designer.cs
@@ -29,15 +29,30 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 435);
+            this.panel1.Size = new System.Drawing.Size(780, 435);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(780, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PROVEEDORES";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // wf_crear_nuevo_proveedor
             // 
@@ -47,6 +62,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "wf_crear_nuevo_proveedor";
             this.Text = "wf_crear_nuevo_proveedor";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -54,5 +70,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
